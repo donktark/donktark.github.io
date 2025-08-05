2dfe86a5ed9ea4851dfe226afcd4f9b89cbd0842
donktark <104886281+donktark@users.noreply.github.com>
2025-08-05 16:37:30 +0900

    versions

--- deploy.bat
+++ deploy.bat
@@ -1,5 +1,5 @@
 hugo
-xcopy /q /E /Y /I C:\Users\32197453\ml-blog\public\* C:\Users\32197453\ml-blog\docs
+xcopy /q /E /Y /I C:\Users\32197453\ml-blog\public\latest\* C:\Users\32197453\ml-blog\docs\latest
 git add .
 git commit -m %1
 git push origin main
