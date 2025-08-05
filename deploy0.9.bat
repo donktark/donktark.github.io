fc134f297e54bb6eb5a8c4d8c3de6ca695a4d5d4
donktark <104886281+donktark@users.noreply.github.com>
2025-08-05 17:38:50 +0900

    deploy v0.9

---
+++ deploy0.9.bat
@@ -0,0 +1,5 @@
+hugo --destination public/v0.9
+xcopy /q /E /Y /I C:\Users\32197453\ml-blog\public\v0.9\* C:\Users\32197453\ml-blog\docs\v0.9
+git add .
+git commit -m "deploy v0.9"
+git push origin main
