bb2da3d2de580a2dd21892a8198f616d04b3307b
donktark <104886281+donktark@users.noreply.github.com>
2025-08-11 14:44:28 +0900

    font size

--- deploy.bat
+++ deploy.bat
@@ -1,6 +1,6 @@
 @echo off
 chcp 65001 >nul
-set /p msg=커밋 메시지를 입력하세요 :
+set /p msg= -- 커밋 메시지를 입력하세요 :
 hugo --destination public/latest
 xcopy /q /E /Y /I C:\Users\32197453\ml-blog\public\latest\* C:\Users\32197453\ml-blog\docs\latest
 git add .
