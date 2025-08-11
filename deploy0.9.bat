e34f23de6a0f97992736071ad72755d53cfc6a6e
donktark <104886281+donktark@users.noreply.github.com>
2025-08-11 10:30:33 +0900

    sidebar font size

--- deploy0.9.bat
+++ deploy0.9.bat
@@ -1,4 +1,4 @@
-hugo --destination public/v0.9
+# hugo --destination public/v0.9
 xcopy /q /E /Y /I C:\Users\32197453\ml-blog\public\v0.9\* C:\Users\32197453\ml-blog\docs\v0.9
 git add .
 git commit -m "deploy v0.9"
--- deploy1.0.bat
+++ deploy1.0.bat
@@ -1,5 +1,5 @@
-
-xcopy /q /E /Y /I C:\Users\32197453\ml-blog\public\v0.9\* C:\Users\32197453\ml-blog\docs\v1.0
+# hugo --destination public/v1.0
+xcopy /q /E /Y /I C:\Users\32197453\ml-blog\public\v1.0\* C:\Users\32197453\ml-blog\docs\v1.0
 git add .
 git commit -m "deploy v1.0"
 git push origin main
