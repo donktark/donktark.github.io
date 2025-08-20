aa892322900cc37f89988f1cb85e060a4a0dab51
donktark <104886281+donktark@users.noreply.github.com>
2025-08-20 09:19:31 +0900

    404 error

--- deploy.bat
+++ deploy.bat
@@ -1,7 +1,7 @@
 @echo off
 chcp 65001 >nul
 set /p msg= -- 커밋 메시지를 입력하세요 :
-hugo --destination public/latest
+hugo --destination public
 xcopy /q /E /Y /I C:\Users\32197453\ml-blog\public\latest\* C:\Users\32197453\ml-blog\docs\latest
 git add .
 git commit -m "%msg%"
